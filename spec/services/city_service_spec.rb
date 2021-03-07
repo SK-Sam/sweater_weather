5783dace7a063803b18fbc2dba77e5e56196d25a
Sam Yeo <yeosamuel95@gmail.com>
2021-03-07 03:40:35 -0700

    Change typos and value expectations

--- spec/services/city_service_spec.rb
+++ spec/services/city_service_spec.rb
@@ -9,26 +9,27 @@ describe 'MapQuest Geocode API based requests' do
         json_data = CityService.get_location_data(city_state_param)
 
         expect(json_data).to have_key(:info)
-        expect(json_data[:data]).to be_a(Hash)
+        expect(json_data[:info]).to be_a(Hash)
 
         expect(json_data).to have_key(:options)
-        expect(json_data[:data]).to be_a(Hash)
+        expect(json_data[:options]).to be_a(Hash)
 
         expect(json_data).to have_key(:results)
-        expect(json_data[:data]).to be_an(Array)
+        expect(json_data[:results]).to be_an(Array)
 
         results_data = json_data[:results].first
 
         expect(results_data).to be_a(Hash)
         expect(results_data[:providedLocation]).to be_a(Hash)
-        expect(results_data[:providedLocation[:location]]).to be_a(String)
-        expect(results_data[:providedLocation[:location]]).to eq('san francisco,ca')
+        expect(results_data[:providedLocation][:location]).to be_a(String)
+        expect(results_data[:providedLocation][:location]).to eq('san%20francisco,ca')
 
-        expect(results_data[:locations]).to have_key(:latLng)
-        expect(results_data[:locations][:latLng]).to be_a(Hash)
-        expect(results_data[:locations][:latLng][:lat]).to be_a(String)
-        expect(results_data[:locations][:latLng][:lat]).to eq('37.78008')
-        expect(results_data[:locations][:latLng][:lng]).to be_a('-122.420168')
+        expect(results_data[:locations].first).to have_key(:latLng)
+        expect(results_data[:locations].first[:latLng]).to be_a(Hash)
+        expect(results_data[:locations].first[:latLng][:lat]).to be_a(Numeric)
+        expect(results_data[:locations].first[:latLng][:lat]).to eq(37.78008)
+        expect(results_data[:locations].first[:latLng][:lng]).to be_a(Numeric)
+        expect(results_data[:locations].first[:latLng][:lng]).to eq(-122.420168)
       end
     end
   end
