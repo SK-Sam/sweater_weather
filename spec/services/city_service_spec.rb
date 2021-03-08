df82e3d6e8cae4466b21ba00f3f2d64bf43ee753
Sam Yeo <yeosamuel95@gmail.com>
2021-03-08 10:59:30 -0700

    Add class method for final map endpoint in cityservice

--- spec/services/city_service_spec.rb
+++ spec/services/city_service_spec.rb
@@ -32,6 +32,21 @@ describe 'MapQuest Geocode API based requests' do
         expect(results_data[:locations].first[:latLng][:lng]).to eq(-122.420168)
       end
     end
+    it '#get_directions_data' do
+      VCR.use_cassette('get_map_data') do
+        direction_data = MunchieService.get_directions_data
+  
+        expect(direction_data).to be_a(Hash)
+        expect(direction_data[:route][:realtime]).to be_a(Numeric)
+        expect(direction_data[:route][:locations]).to be_an(Array)
+        expect(direction_data[:route][:locations].last).to have_key(:adminArea3)
+        expect(direction_data[:route][:locations].last).to have_key(:adminArea5)
+        expect(direction_data[:route]).to have_key(:formattedTime)
+        expect(direction_data[:route][:locations].last).to have_key(:displayLatLng)
+        expect(direction_data[:route][:locations].last[:displayLatLng]).to have_key(:lat)
+        expect(direction_data[:route][:locations].last[:displayLatLng]).to have_key(:lng)
+      end
+    end
   end
   describe 'sad path' do
 
