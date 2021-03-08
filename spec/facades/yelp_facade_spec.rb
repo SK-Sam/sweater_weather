de123b26825e64f770f54bc5afbfcccf4f42c468
Sam Yeo <yeosamuel95@gmail.com>
2021-03-08 11:36:07 -0700

    Change data type

--- spec/facades/yelp_facade_spec.rb
+++ spec/facades/yelp_facade_spec.rb
@@ -6,7 +6,7 @@ describe 'YelpFacade' do
       restaurant_data = YelpFacade.filter_restaurant_data('38.265427', '-104.605087', 'hamburger', 1615231953)
       
       expect(restaurant_data[:name]).to be_a(String)
-      expect(restaurant_data[:address]).to be_a(Float)
+      expect(restaurant_data[:address]).to be_a(String)
     end
   end
 end
