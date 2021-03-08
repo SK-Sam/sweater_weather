3198361d99d09a5b141610929d976148f9d4e5fa
Sam Yeo <yeosamuel95@gmail.com>
2021-03-08 11:35:15 -0700

    Create test for Yelp Facade

---
+++ spec/facades/yelp_facade_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe 'YelpFacade' do
+  it '#filter_restaurant_data' do
+    VCR.use_cassette('final_yelp_fixture') do
+      restaurant_data = YelpFacade.filter_restaurant_data('38.265427', '-104.605087', 'hamburger', 1615231953)
+      
+      expect(restaurant_data[:name]).to be_a(String)
+      expect(restaurant_data[:address]).to be_a(Float)
+    end
+  end
+end
