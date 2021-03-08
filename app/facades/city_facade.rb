1259a74bb184f913a50ec39dcaeacd18d73e1a58
Sam Yeo <yeosamuel95@gmail.com>
2021-03-08 11:14:34 -0700

    Filter directions data in city facade

--- app/facades/city_facade.rb
+++ app/facades/city_facade.rb
@@ -7,5 +7,16 @@ class CityFacade
         lon: location_data[:latLng][:lng].to_s
       }
     end
+    
+    def filter_direction_data(start, fin)
+      map_data = CityService.get_directions_data(start, fin)
+      travel_time_with_traffic = map_data[:route][:realTime]
+  
+      destination_city = map_data[:route][:locations].last[:adminArea5] +
+      ', ' + map_data[:route][:locations].last[:adminArea3]
+      travel_time = map_data[:route][:formattedTime].first(2) + ' hours ' + map_data[:route][:formattedTime][3..4] + ' min'
+      lat = map_data[:route][:locations].last[:displayLatLng][:lat]
+      lng = map_data[:route][:locations].last[:displayLatLng][:lng]
+    end
   end
 end
