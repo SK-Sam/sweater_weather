0a96b39c279f9fda79dd00abc632d8474107213e
Sam Yeo <yeosamuel95@gmail.com>
2021-03-09 02:57:04 -0700

    Create Image Poro

---
+++ app/poros/image.rb
@@ -0,0 +1,15 @@
+class Image
+  attr_reader :image, :credit
+
+  def initialize(data, location)
+    @image = {
+      location: location,
+      image_url: data[:results].first[:urls][:full]
+    }
+    @credit = {
+      source: 'https://unsplash.com',
+      author_username: data[:results].first[:user][:username],
+      author_name: data[:results].first[:user][:name]
+    }
+  end
+end
