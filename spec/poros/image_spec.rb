686b0f6c55df36ec3ac5430e78a3c07be27f2678
Sam Yeo <yeosamuel95@gmail.com>
2021-03-09 02:56:44 -0700

    Change key names

--- spec/poros/image_spec.rb
+++ spec/poros/image_spec.rb
@@ -2,16 +2,20 @@ require 'rails_helper'
 
 describe 'Image Poro for background image' do
   it 'can initialize with proper attributes based on data passing in' do
-    data = 
+    VCR.use_cassette('get_san_fran_unsplash_image') do
+      location_param = 'san francisco,ca'
+      data = BackgroundService.get_image(location_param)
 
-    image_poro = Image.new(data)
+      image_poro = Image.new(data, 'san francisco,ca')
 
-    expect(image_poro.image).to be_a(Hash)
-    expect(image_poro.image[:location]).to be_a(String)
-    expect(image_poro.image[:image_url]).to be_a(String)
+      expect(image_poro.image).to be_a(Hash)
+      expect(image_poro.image[:location]).to be_a(String)
+      expect(image_poro.image[:image_url]).to be_a(String)
 
-    expect(image_poro.credit).to be_a(Hash)
-    expect(image_poro.credit[:source]).to be_a(String)
-    expect(image_poro.credit[:author]).to be_a(String)
+      expect(image_poro.credit).to be_a(Hash)
+      expect(image_poro.credit[:source]).to be_a(String)
+      expect(image_poro.credit[:author_username]).to be_a(String)
+      expect(image_poro.credit[:author_name]).to be_a(String)
+    end
   end
 end
