5e8d6cefdc4422ba8d556c7eeb831f38ffcb20ff
Sam Yeo <yeosamuel95@gmail.com>
2021-03-09 22:55:12 -0700

    Create route for login

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       get '/forecast', to: 'forecast#city_weather'
       get '/backgrounds', to: 'background#city_image'
       post '/users', to: 'users#create'
+      post '/sessions', to: 'sessions#create'
     end
   end
 end
