0fb180a94b68c419a8d2bcb72630a3c8f1952655
Sam Yeo <yeosamuel95@gmail.com>
2021-03-09 21:16:33 -0700

    Create user creation spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,15 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :password_digest }
     it { should validate_uniqueness_of :email }
   end
+  describe 'Attributes' do
+    it 'can instantiate with attributes' do
+      email = 'test@test.com'
+      password = 'password'
+      user = User.create(email: email, password: password, password_confirmation: password)
+
+      expect(user.email).to eq(email)
+      expect(user.password).to be_a(String)
+      expect(user.api_key).to be_a(String)
+    end
+  end
 end
