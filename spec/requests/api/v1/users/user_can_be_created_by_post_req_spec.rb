46584adbb699f59d125b90e19b4b15122272b3a8
Sam Yeo <yeosamuel95@gmail.com>
2021-03-09 21:56:14 -0700

    Correct typos

--- spec/requests/api/v1/users/user_can_be_created_by_post_req_spec.rb
+++ spec/requests/api/v1/users/user_can_be_created_by_post_req_spec.rb
@@ -2,17 +2,25 @@ require 'rails_helper'
 
 describe 'User Create Request' do
   it 'can create user through JSON body request' do
-    post '/api/v1/users'
+    headers = {
+      'Content_Type': 'application/json'
+    }
+    body = {
+      'email': 'email@email.com',
+      'password': 'password',
+      'password_confirmation': 'password'
+    }
+    post '/api/v1/users', headers: headers, params: body
     user = User.first
 
     expect(response.status).to eq(201)
     
     json_body = JSON.parse(response.body, symbolize_names: true)[:data]
 
-    expect(json_body[:id]).to eq(user.id)
+    expect(json_body[:id]).to eq(user.id.to_s)
     expect(json_body[:type]).to eq('users')
     expect(json_body[:attributes]).to be_a(Hash)
-    expect(json_body[:attributes[:email]]).to eq(user.email)
+    expect(json_body[:attributes][:email]).to eq(user.email)
     expect(json_body[:attributes][:api_key]).to eq(user.api_key)
   end
 end
