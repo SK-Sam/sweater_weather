35e6014b6384d8dcfe3d7eedece3d6e7b18736c6
Sam Yeo <yeosamuel95@gmail.com>
2021-03-10 06:39:50 -0700

    Create sad paths

--- app/controllers/api/v1/background_controller.rb
+++ app/controllers/api/v1/background_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::BackgroundController < ApplicationController
   def city_image
-    location_data = BackgroundService.get_image(params[:location])
-    filtered_image_data = Image.new(location_data, params[:location])
-    render json: ImageSerializer.new(filtered_image_data)
+    if params[:location]
+      location_data = BackgroundService.get_image(params[:location])
+      filtered_image_data = Image.new(location_data, params[:location])
+      render json: ImageSerializer.new(filtered_image_data)
+    else
+      render json: { errors: "Location invalid. Please put in a valid location." }, status: 400
+    end
   end
 end
