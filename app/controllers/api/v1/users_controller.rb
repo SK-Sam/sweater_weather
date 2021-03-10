25862f07ffb691696e1c78a434c2f31d61a012fc
Sam Yeo <yeosamuel95@gmail.com>
2021-03-10 00:25:30 -0700

    Add sad path to user creation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,15 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    user = User.create(
+    user = User.new(
       email: params[:email],
       password: params[:password],
       password_confirmation: params[:password_confirmation]
     )
-    if user
+    if user.save
       session[:user_id] = user.id
       render json: UsersSerializer.new(user), status: :created
     else
-      render json: { status: 400 }
+      render json: { errors: "Please try again. Issue with creating your account." }, status: 400
     end
   end
 
